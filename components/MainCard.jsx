ab09dc74f7048cb153e9b1c1221d30ce50c931f7
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2022-01-19 14:17:02 -0300

    change colors, bordr main out

--- components/MainCard.jsx
+++ components/MainCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainCard = ({src, alt, title}) => {
     return (
-        <div className='m-2 py-4 mx-0 text-center border-2 border-gray rounded-xl shadow-gray shadow-inner'>
+        <div className='m-2 py-4 mx-0 text-center'>
             <div className='mx-2'>
                 <img className='mx-auto h-16 w-16' src={src} alt={alt} />
                 <p className='text-dgray text-center text-base uppercase font-semibold mt-2 mx-'>{title}</p>
